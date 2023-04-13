485a8433cea9171c408f8701664335012252f839
Sumen Tse <sumen.tse@gmail.com>
2023-04-13 16:56:06 -0400

    adjust table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -22,9 +22,14 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-const Table = ({ columns, rows, rowsPerPageOptions }) => {
+const Table = ({ dataId, columns, rows, rowsPerPageOptions, disablePagination, tableContainerStyles }) => {
+
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions?.[0] || 5);
+
+  const start = page * rowsPerPage;
+  const end = start + rowsPerPage;
+  const paginatedRows = rows?.slice(start, end);
 
   const handleChangePage = (_, newPage) => {
     setPage(newPage);
@@ -35,47 +40,89 @@ const Table = ({ columns, rows, rowsPerPageOptions }) => {
     setPage(0);
   };
 
-  const start = page * rowsPerPage;
-  const end = start + rowsPerPage;
-  const paginatedRows = rows?.slice(start, end);
+  const handleCellRender = ({ value, formatter }) => {
+    if (formatter) {
+      return formatter(value);
+    }
+    return value;
+  };
+
+  const handleRowRender = () => {
+    if (disablePagination) {
+      return rows.map((row, index) => {
+        return (
+          <StyledTableRow key={index}>
+            {columns.map((column) => {
+              return (
+                <TableCell key={`${index}-${column.field}`}>
+                  {handleCellRender({
+                    value: row?.[column?.field],
+                    formatter: column?.valueFormatter,
+                  })}
+                </TableCell>
+              );
+            })}
+          </StyledTableRow>
+        );
+      });
+    }
+    return paginatedRows.map((row, index) => (
+      <StyledTableRow key={index}>
+        {columns.map((column) => {
+          return (
+            <TableCell key={`${index}-${column.field}`}>
+              {handleCellRender({
+                value: row?.[column?.field],
+                formatter: column?.valueFormatter,
+              })}
+            </TableCell>
+          );
+        })}
+      </StyledTableRow>
+    ));
+  };
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <MaterialTable aria-label="custom-table">
+      <TableContainer data-id={dataId} component={Paper} sx={{ maxHeight: 400, ...tableContainerStyles }}>
+        <MaterialTable
+          aria-label="custom-table"
+          sx={{ borderCollapse: "unset" }}
+        >
           <TableHead>
             <TableRow>
               {columns.map((column) => (
-                <TableCell key={`${column.field}-header`}>
+                <TableCell
+                  key={`${column.field}-header`}
+                  sx={{
+                    borderBottom: "solid black 1px",
+                    position: "sticky",
+                    bgcolor: "white",
+                    zIndex: 1,
+                    top: 0,
+                  }}
+                >
                   {column.headerName}
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
-          <TableBody>
-            {paginatedRows.map((row) => (
-              <StyledTableRow key={row.id}>
-                {columns.map((column) => {
-                  return (
-                    <TableCell key={`${row.id}-${column.field}`}>
-                      {row?.[column?.field] || ""}
-                    </TableCell>
-                  );
-                })}
-              </StyledTableRow>
-            ))}
+          <TableBody style={{ overflow: "auto", height: "100" }}>
+            {handleRowRender()}
           </TableBody>
         </MaterialTable>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={rowsPerPageOptions || [5, 10, 25]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+      {!disablePagination && (
+        <TablePagination
+          rowsPerPageOptions={rowsPerPageOptions || [5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      )}
     </>
   );
 };
