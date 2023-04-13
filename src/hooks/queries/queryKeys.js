09d0115169a8a8313baae053c6d9c4cfceb73d81
Sumen Tse <sumen.tse@gmail.com>
2023-04-12 20:47:11 -0400

    fix query keys

--- src/hooks/queries/queryKeys.js
+++ src/hooks/queries/queryKeys.js
@@ -1,9 +1,9 @@
 const lendingQueryKeys = {
   all: ["loans"],
-  listUsers: () => [...loans.all, "users"],
-  getUserLoans: (userId) => [...loans.listUsers(), userId, "loans"],
-  getLoanDetails: (loanId) => [...loans.all, loanId],
-  getLoanByMonth: (loanId, month) => [...loans.getLoanDetails(loanId), "month", month],
+  listUsers: () => [...lendingQueryKeys.all, "users"],
+  getUserLoans: (userId) => [...lendingQueryKeys.listUsers(), userId, "loans"],
+  getLoanDetails: (loanId) => [...lendingQueryKeys.all, loanId],
+  getLoanByMonth: (loanId, month) => [...lendingQueryKeys.getLoanDetails(loanId), "month", month],
 };
 
 export default lendingQueryKeys;
